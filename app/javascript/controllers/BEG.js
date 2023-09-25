5bcf12e2289142a68c6e38a680ce078d2952af2a
Luciano Scarpaci <tauxsik@gmail.com>
2023-09-25 12:07:51 -0400

    Code Formatting

--- app/javascript/controllers/BEG.js
+++ app/javascript/controllers/BEG.js
@@ -4,19 +4,19 @@ const { Go1, Go1Mode } = require("@droneblocks/go1-js");
 //BEG Mode has to wait 6 seconds
 
 class myClient {                                                                                                                                           
-    constructor() {                                                                                                                                          
-      this.robotDog = new Go1();
-      this.robotDog.init();                                                                                                                             
-    }
+  constructor() {                                                                                                                                          
+    this.robotDog = new Go1();
+    this.robotDog.init();                                                                                                                             
+  }
 
-    async begMode() {
-      for (let i = 0; i < 2; i++) {
-        await this.robotDog.wait(1000);
-        this.robotDog.setMode(Go1Mode.straightHand1);
-        await this.robotDog.wait(3000);
-      }
+  async begMode() {
+    for (let i = 0; i < 2; i++) {
+      await this.robotDog.wait(1000);
+      this.robotDog.setMode(Go1Mode.straightHand1);
       await this.robotDog.wait(3000);
-	}
+    }
+    await this.robotDog.wait(3000);
+  }
 }
 
 const client = new myClient();
