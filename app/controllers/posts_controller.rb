bed36b5938808686c3e6fa1398312e468460104f
bwetzel2016 <105940974+bwetzel2016@users.noreply.github.com>
2023-10-05 15:47:23 -0400

    User Authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+ 
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -12,7 +14,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   # GET /posts/1/edit
@@ -21,7 +23,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
@@ -57,6 +59,15 @@ class PostsController < ApplicationController
     end
   end
 
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to posts_path, notice: "Not Authorized to Edit This Appoitment" if @post.nil?
+  end
+
+
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
@@ -65,6 +76,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:first_name, :last_name, :znumber, :email, :advisor, :date, :time)
+      params.require(:post).permit(:first_name, :last_name, :znumber, :email, :advisor, :date, :time, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_29_201652) do
+ActiveRecord::Schema.define(version: 2023_10_05_183727) do
 
   create_table "lists", force: :cascade do |t|
     t.string "first_name"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2023_08_29_201652) do
     t.time "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
